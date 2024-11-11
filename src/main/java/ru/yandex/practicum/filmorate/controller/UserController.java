f39128db8e0ba59875ef521a4f2d712cc273afc6
Maxim <vatruh77@yandex.ru>
2024-11-11 13:32:58 +0300

    Добавить функционал "Рекомендации"

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -5,19 +5,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Event;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.Collection;
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
+    private final FilmService filmService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, FilmService filmService) {
         this.userService = userService;
+        this.filmService = filmService;
     }
 
     @GetMapping
@@ -75,5 +80,10 @@ public class UserController {
     @GetMapping("{id}/feed")
     public Collection<Event> getEvents(@PathVariable long id) {
         return userService.getEvents(id);
+    }
+
+    @GetMapping("/{id}/recommendations")
+    public List<Film> getRecommendation(@PathVariable long id) {
+        return filmService.getRecommendation(id);
     }
 }
